9d8815de1988dfc9414767bf10896af52586d990
JayceLai <goodlaijiaxin@qq.com>
2020-09-08 09:30:10 +0800

    tweaks

--- make.sh
+++ make.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+base_dir=$(cd "$(dirname "$0")";pwd)
+echo $base_dir
 cd node_modules/physx/physx/compiler/emscripten-release/
 make
-mkdir -p /src/dist
-cp /src/node_modules/physx/physx/bin/emscripten/release/physx.release.js /src/dist/physx.release.js
-cp /src/node_modules/physx/physx/bin/emscripten/release/physx.release.wasm /src/dist/physx.release.wasm
+mkdir -p $base_dir/src/dist
+cp $base_dir/node_modules/physx/physx/bin/emscripten/release/physx.release.js $base_dir/src/dist/physx.release.js
+cp $base_dir/node_modules/physx/physx/bin/emscripten/release/physx.release.wasm $base_dir/src/dist/physx.release.wasm
