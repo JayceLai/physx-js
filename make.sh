4a1f60c6b4ea6efd7be97bd77d19a1e4778e6546
JayceLai <goodlaijiaxin@qq.com>
2020-09-29 16:54:25 +0800

    stash

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ base_dir=$(cd "$(dirname "$0")";pwd)
 echo $base_dir
 cd node_modules/physx/physx/compiler/emscripten-release/
 make
-mkdir -p $base_dir/src/dist
-cp $base_dir/node_modules/physx/physx/bin/emscripten/release/physx.release.asm.js $base_dir/src/dist/physx.release.asm.js
-cp $base_dir/node_modules/physx/physx/bin/emscripten/release/physx.release.wasm.js $base_dir/src/dist/physx.release.wasm.js
-cp $base_dir/node_modules/physx/physx/bin/emscripten/release/physx.release.wasm.wasm $base_dir/src/dist/physx.release.wasm.wasm
+mkdir -p $base_dir/builds
+cp $base_dir/node_modules/physx/physx/bin/emscripten/release/physx.release.asm.js $base_dir/builds/physx.release.asm.js
+cp $base_dir/node_modules/physx/physx/bin/emscripten/release/physx.release.wasm.js $base_dir/builds/physx.release.wasm.js
+cp $base_dir/node_modules/physx/physx/bin/emscripten/release/physx.release.wasm.wasm $base_dir/builds/physx.release.wasm.wasm
